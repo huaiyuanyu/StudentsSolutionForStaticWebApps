068e6818adb2db252869ab316e0fb24b404cf068
Gary Yu <hyu100@my.bcit.ca>
2025-02-22 21:05:30 -0800

    first commit

---
+++ StudentLibrary/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentLibrary;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string? LastName { get; set; } = null!;
+
+    public string? FirstName { get; set; } = null!;
+
+    public string School { get; set; } = null!;
+}
+
